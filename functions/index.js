f8a51940f27bbbf9a67448483f28dfb279cd9648
Vibodh Bhosure <vibodhbhoure@gmail.com>
2022-12-06 14:01:05 +0530

    specific time get route added

--- functions/index.js
+++ functions/index.js
@@ -30,8 +30,8 @@ app.get("/api/:apiKey/getData", (req, res) => {
 
     const apiAuth = req.params.apiKey;
 
-    if (apiAuth != "Thisisourapikey") {
-        res.status(403).sendFile(__dirname + "/failure.html");
+    if (apiAuth != "KeyA20222023") {
+        res.sendFile(__dirname + "/failure.html");
     }
 
     let co2 = [];
@@ -95,6 +95,118 @@ app.get("/api/:apiKey/getData", (req, res) => {
             res.status(200).send(datatoSend);
         });
 });
+
+app.get("/api/:apiKey/getData/:unix/adjustTime/:adjustTime", (req, res) => {
+    const adjustTime = Number(req.params.adjustTime);
+    const unixTime = Number(req.params.unix);
+    const apiAuth = req.params.apiKey;
+
+    const adjustUnixTimeUp = unixTime + 60 * adjustTime;
+    const adjustUnixTimeDown = unixTime - 60 * adjustTime;
+
+    if (apiAuth != "KeyA20222023") {
+        res.sendFile(__dirname + "/failure.html");
+    }
+
+    let co2 = [];
+    let dust = [];
+    let epoch = [];
+    let eth = [];
+    let h2 = [];
+    let hum = [];
+    let lat = [];
+    let lon = [];
+    let mq135 = [];
+    let o3 = [];
+    let temp = [];
+    let voc = [];
+    let indexArr = [];
+
+    let Newco2 = [];
+    let Newdust = [];
+    let Newepoch = [];
+    let Neweth = [];
+    let Newh2 = [];
+    let Newhum = [];
+    let Newlat = [];
+    let Newlon = [];
+    let Newmq135 = [];
+    let Newo3 = [];
+    let Newtemp = [];
+    let Newvoc = [];
+
+    Todo.ref("Todo").once("value")
+        .then((snapshot) => {
+            const data = snapshot.val();
+            Object.entries(data).forEach((element) => {
+                co2.push(element[1].co2);
+                dust.push(element[1].dust);
+                epoch.push(element[1].epoch);
+                h2.push(element[1].h2);
+                eth.push(element[1].eth);
+                hum.push(element[1].hum);
+                lat.push(element[1].lat);
+                lon.push(element[1].lon);
+                mq135.push(element[1].mq135);
+                o3.push(element[1].o3);
+                temp.push(element[1].temp);
+                voc.push(element[1].voc);
+            });
+
+
+            epoch.forEach((item, index) => {
+                item = Number(item);
+                if (item >= adjustUnixTimeDown && item <= adjustUnixTimeUp) {
+                    indexArr.push(index);
+                }
+            });
+
+            indexArr.forEach((item) => {
+                Newco2.push(co2[item]);
+                Newdust.push(dust[item]);
+                Newepoch.push(epoch[item]);
+                Newh2.push(h2[item]);
+                Neweth.push(eth[item]);
+                Newhum.push(hum[item]);
+                Newlat.push(lat[item]);
+                Newlon.push(lon[item]);
+                Newmq135.push(mq135[item]);
+                Newo3.push(o3[item]);
+                Newtemp.push(temp[item]);
+                Newvoc.push(voc[item]);
+            });
+
+            let newEpoch = [];
+
+            Newepoch.forEach((item, index) => {
+                item = Number(item);
+                item += 19800;
+                var date = new Date(item * 1000);
+                var hours = date.getHours();
+                var minutes = "0" + date.getMinutes();
+                var seconds = "0" + date.getSeconds();
+                item = date + "-" + hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+                newEpoch[index] = item;
+            });
+
+            const datatoSend = {
+                co2: Newco2.map(Number),
+                dust: Newdust.map(Number),
+                epoch: newEpoch,
+                h2: Newh2.map(Number),
+                eth: Neweth.map(Number),
+                hum: Newhum.map(Number),
+                lat: Newlat.slice(-1)[0],
+                lon: Newlon.slice(-1)[0],
+                mq135: Newmq135.map(Number),
+                o3: Newo3.map(Number),
+                temp: Newtemp.map(Number),
+                voc: Newvoc.map(Number)
+            }
+            res.status(200).send(datatoSend);
+        });
+});
+
 
 // app.listen(process.env.PORT || 4000, () => {
 //     console.log("Server is running at port 4000");
